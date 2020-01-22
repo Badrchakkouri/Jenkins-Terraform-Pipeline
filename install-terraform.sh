874f655f87c6be29295069c5aa949296e71e75f1
Badr Chakkouri <badr.chakkouri@gmail.com>
2020-01-22 18:23:32 +0100

    Second commit

---
+++ install-terraform.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y unzip
+sudo apt-get install -y wget
+sudo wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_windows_amd64.zip
+sudo unzip terraform_0.12.19_windows_amd64.zip
+sudo mv terraform /usr/local/bin/
