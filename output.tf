9405c7d4f27f70056d02de01998261ca6798443c
Badr Chakkouri <badr.chakkouri@gmail.com>
2020-01-23 11:06:20 +0100

    3rd commit

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "public_ip" {
-  value = aws_instance.refresh_instance.public_ip
+  value = "use this link to access the web page: http://"+aws_instance.refresh_instance.public_ip+"/index.html"
 }
